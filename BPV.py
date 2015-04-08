4a906d1576cc8bbb1e4ae67cb8d4df17c9fa5fa9
renato <renato@zuko>
2015-04-08 14:25:44 +0200

    first implemention of dynamic programming PTAS "done right", though it doesn't work ATM

--- BPV.py
+++ BPV.py
@@ -2,7 +2,9 @@ import numpy as np
 import queue
 import pulp
 import timeit
+import heapq
 import ipdb
+import pdb
 
 def relative_error(approximated_instance, exact_instance):
     if approximated_instance.solved() and exact_instance.solved():
@@ -87,9 +89,26 @@ def is_valid_node_shape(shape,node):
         return(0)
     else:
         return(1)    
+
+class dyn_prog_graph_node:
+    def __init__(self,coords):
+        self.coords = tuple(coords)
+
+    def __eq__(self, other):
+        return (self.coords == other.coords)
     
+    def __lt__(self, other):
+        if type(other) != type(self):
+            raise NotImplementedError("Can only compare two dyn_prog_graph_node")
+        if self.coords[0] == other.coords[0] and self.coords[1] == other.coords[1] and self.coords[2] != other.coords[2]:
+            raise RuntimeError("Two nodes can't differ only in the last coordinate")
+        else:
+            selfinvcoords = (self.coords[0], -self.coords[1], self.coords[2])
+            otherinvcoords = (other.coords[0], -other.coords[1], other.coords[2])
+            return (selfinvcoords < otherinvcoords) #uses lexicographic ordering
+            
 class BPV:
-    def __init__(self,solver_type,tot_patterns,max_cardinality,max_rate,p,dynprog_significant_figures=3):
+    def __init__(self,solver_type,tot_patterns,max_cardinality,max_rate,p):
         if len(p) != tot_patterns:
             err = "len(p) == tot_patterns must hold"
             raise RuntimeError(err)
@@ -97,17 +116,16 @@ class BPV:
             self.__is_solved__ = 0
             self.__all_solvers__ = {"exact": self.exact_solver, "euristic": self.euristic_solver, "dynprog": self.dynprog_solver}
             self.__solver_type__ = None
-            self.__dynprog_significant_figures__ = dynprog_significant_figures
             self.tot_patterns = tot_patterns
             self.max_cardinality = max_cardinality
             self.max_rate = max_rate
             self.p = p
             self.plog1onp = self.p*np.log(1/self.p)
             self.set_solver(solver_type)
+            self.tot_entropy = self.plog1onp.sum()
 
 
     def set_solver(self, solver_type):
-        #print(self.tot_patterns)
         if solver_type not in self.__all_solvers__.keys():
             err = ("solver_type has to be one of"+" \"%s\""*len(solvers) % solvers.keys())
             raise RuntimeError(err)
@@ -132,7 +150,7 @@ class BPV:
     def solver(self):
         return self.__solver_type__
 
-    def solve(self):
+    def solve(self, epsilon=0.05):
         ans=None
         if self.solved() == 1:
             ans = input("Problem is allready solved, solve again? y/N")
@@ -140,7 +158,10 @@ class BPV:
             if self.solver() == None:
                 print("ERROR: set solver type with set_solver")
             else:
-                self.__all_solvers__[self.solver()]()
+                if self.solver() == "dynprog":
+                    self.__all_solvers__["dynprog"](epsilon)
+                else:
+                    self.__all_solvers__[self.solver()]()
                 self.__is_solved__ = 1
 
 
@@ -266,112 +287,89 @@ class BPV:
                 self.__solution_indexes__.append(arg_max)
                 self.__solution_cardinality__ += 1
 
-    def dynprog_solver(self):
+    def dynprog_solver(self,epsilon):
         """Calculates the solution using Dynamic Programming and Bellman's shortest path algorithm"""
 
-        #self.dynprog_table_dim = (self.tot_patterns+1,int(self.max_rate*(10**self.__dynprog_significant_figures__))+1,self.max_cardinality+1)
-        #self.dynprog_table = np.zeros(self.dynprog_table_dim) #TODO: would it be better to implement the table as a dictionary?
-        self.dynprog_table = {}
-        self.dynprog_scaled_p = np.zeros(self.tot_patterns,dtype='int')
-        self.dynprog_approx_plog1onp = np.zeros(self.tot_patterns)
+        scaling_factor = epsilon*self.plog1onp[-1]/self.tot_patterns
+        print("epsilon: ", epsilon, "scaling factor: ", scaling_factor)
+        scaled_plog1onp = np.zeros(self.tot_patterns)
         for i in range(self.tot_patterns):
-            self.dynprog_scaled_p[i] = int(self.p[i]*(10**self.__dynprog_significant_figures__)) #can't do int() on numpy array
-            approx_pi = 10**-self.__dynprog_significant_figures__*self.dynprog_scaled_p[i]
-            if approx_pi == 0:
-                self.dynprog_approx_plog1onp[i] = 0
-            else:
-                self.dynprog_approx_plog1onp[i] = approx_pi*np.log(1/approx_pi)
+            scaled_plog1onp[i] = 1 + int(self.plog1onp[i]/scaling_factor)
+        scaled_tot_entropy = scaled_plog1onp.sum()
 
-        node_queue = queue.Queue()
-        inqueue = set()
-        root = (self.tot_patterns,int(self.max_rate*(10**self.__dynprog_significant_figures__)),self.max_cardinality)
-        table_shape = (root[0] + 1, root[1] + 1, root[2] + 1)
-        self.dynprog_table[root] = 0
-        #root = (self.dynprog_table_dim[0]-1,self.dynprog_table_dim[1]-1,self.dynprog_table_dim[2]-1)
-        node_queue.put(root)
-        inqueue.add(root)
-        
+        #pdb.set_trace()
+        table = {}
+        table_shape = (self.tot_patterns, scaled_tot_entropy, self.max_cardinality)
+        heap = []
+        heapq.heapify(heap)
+        root = dyn_prog_graph_node((0,0,0))
+        heapq.heappush(heap, root)
+        table[root.coords] = 0
+        self.dynprog_best_value = 0
+        self.dynprog_best_value_node = dyn_prog_graph_node((-1,-1,-1))
         predecessor = {}
-        best_node_value = 0
-        best_node_index = None
         
-        while(node_queue.empty() == False):
-            node = node_queue.get(block=False)
-            #ipdb.set_trace()
-            inqueue.remove(node)
-            i,j,k = node
-            #print("node = ", node, "scaled_p[i-1] = ", self.dynprog_scaled_p[i-1], "plog1onp = ", self.dynprog_approx_plog1onp[i-1])
-            #scaled_pi = int(self.p[i-1]*(10**self.__dynprog_significant_figures__)) #worst case: I'm calculating the same value #nodes times = tot_patterns*max_cardinality*max_rate*10**sig_fig
-            child1 = (i - 1, j, k)
-            child2 = (i - 1, j - self.dynprog_scaled_p[i-1], k - 1)
-            if self.dynprog_scaled_p[i-1]  > j:
-                if is_valid_node_shape(table_shape,child1):
-                    predecessor[child1] = node
-                    self.dynprog_table[child1] = self.dynprog_table[node]
+        def check_child(parent, child, arc_type):
+            "Looks at child and if feasible ad.coordsds it to queue"
+            if arc_type not in [1,2]:
+                raise RuntimeError("arc_type must be either 1 or 2")
+            updated = 0
+            if child.coords in predecessor.keys():
+                existent_rate = table[child.coords]
+                if arc_type == 1:
+                    candidate_new_rate = table[parent.coords]
+                elif arc_type == 2:
+                    candidate_new_rate = table[parent.coords] + self.p[child.coords[1] - 1]
+                if candidate_new_rate < existent_rate:
+                    predecessor[child.coords] = parent
+                    table[child.coords] = candidate_new_rate
+                    updated = 1
             else:
-                if is_valid_node_shape(table_shape,child1) and (child1 not in self.dynprog_table.keys() or self.dynprog_table[node] <= self.dynprog_table[child1]):
-                    predecessor[child1] = node
-                    self.dynprog_table[child1] = self.dynprog_table[node]
-                    if child1 not in inqueue: #this is probably uneccessary, as I don't think 'child1 in queue' can ever hold
-                        node_queue.put(child1)
-                        inqueue.add(child1)
-                #if is_valid_node_shape(self.dynprog_table,child2) and self.dynprog_table[node] - self.p[i-1]*np.log(1/self.p[i-1]) < self.dynprog_table[child2]:
-                if is_valid_node_shape(table_shape,child2) and (child2 not in self.dynprog_table.keys() or self.dynprog_table[node] - self.dynprog_approx_plog1onp[i-1] <= self.dynprog_table[child2]):
-                    self.dynprog_table[child2] = self.dynprog_table[node] - self.dynprog_approx_plog1onp[i-1]
-                    predecessor[child2] = node
-                    if child2 not in inqueue:
-                        node_queue.put(child2) 
-                        inqueue.add(child2)
-                    if self.dynprog_table[child2] < best_node_value:
-                        best_node_value = self.dynprog_table[child2]
-                        best_node_index = child2
-                        #print("scaled p[i-1] = ", self.dynprog_scaled_p[i-1])
-                        #self.dynprog_print_path_on_table(predecessor,best_node_index)
-                        #print("--------\n")
-                        #print("new best_node_index = ", child2, "with value = ", self.dynprog_table[child2])
-                        
-
-
-        #for i,v in np.ndenumerate(self.dynprog_table):
-        #    if v != 0:
-        #        print("index = ", i, " value = ", v)
-        
-        ##check to be sure best_node_index was calculated correctly
-        #min = 0
-        #argmin = None
-        #for (index,value) in np.ndenumerate(self.dynprog_table):
-        #    if is_extreme_node(self.dynprog_table, index) and self.dynprog_table[index] < min:
-        #        min = self.dynprog_table[index]
-        #        argmin = index
-        #print("best_node_index = ", best_node_index, "argmin = ", argmin)
+                predecessor[child.coords] = parent
+                table[child.coords] = table[parent.coords]
+                heapq.heappush(heap, child)
+                updated = 1
                 
-        node = best_node_index
-        #print("bni: ", best_node_index, "\nsucc: ",  predecessor[best_node_index],"\n",self.p[0], self.p[1])
+            if updated == 1 and child.coords[1] > self.dynprog_best_value:
+                    self.dynprog_best_value = child.coords[1]
+                    self.dynprog_best_value_node = child
+        
+        while heapq.nlargest(1,heap) != []:
+            cur = heapq.heappop(heap)
+            i,j,k = cur.coords
+            child1 = (i+1,j,k+1)
+            child2 = (i+1, j+scaled_plog1onp[i], k+1)
+            if is_valid_node_shape(table_shape,child1):
+                check_child(cur, dyn_prog_graph_node(child1), 1)
+            if is_valid_node_shape(table_shape,child2) and table[cur.coords] + self.p[i] <= self.max_rate:
+                check_child(cur, dyn_prog_graph_node(child2),2)
+    
         self.__solution_indexes__ = []
         self.__solution_cardinality__ = 0
         self.__solution_rate__ = 0
         self.__solution_entropy__ = 0
-        rounded_entropy = 0
-        #count = 0
-        #print("--------")        
+        intable_entropy = self.dynprog_best_value
+        cur = self.dynprog_best_value_node
+
         while 1:
             try:
-                pred = predecessor[node]
+                next = predecessor[cur.coords]
             except KeyError:
                 break
-            if pred[2] != node[2]:
-                i = pred[0] - 1
-                #print("taking pattern %d with probability %f" % (i, self.p[i]))
-                self.__solution_indexes__.append(i)
+            if cur.coords[1] != next.coords[1]:
+                i = cur.coords[0]
+                self.__solution_indexes__.append(i-1)
                 self.__solution_cardinality__ += 1
-                self.__solution_rate__ += self.p[i]
-                #self.__solution_entropy__ += self.p[i]*np.log(1/self.p[i])
-                self.__solution_entropy__ += self.dynprog_approx_plog1onp[i]
-                #rounded_entropy += self.dynprog_approx_plog1onp[i]
-            node = pred
+                self.__solution_rate__ += self.p[i-1]
+                self.__solution_entropy__ += self.plog1onp[i-1]
+            if next == root:
+                break
+            else:
+                cur = next
 
-        #print("\n-best_node_value = ", -best_node_value*10**-self.__dynprog_significant_figures__,"entropy = ",self.__solution_entropy__, "rounded entropy = ", rounded_entropy, " rate = ", self.__solution_rate__)
-
+        print("in table: ", intable_entropy , "  calculated (scaled): ", 1 + int(self.__solution_entropy__/scaling_factor),\
+              " calculated: ", self.__solution_entropy__)
+                
     def dynprog_print_path_on_table(self,predecessor_dictionary, starting_node):
         n = starting_node
         rate = 0
--- class_test.py
+++ class_test.py
@@ -6,37 +6,43 @@ import timeit
 import pdb
 
 def main():
-    M=500  #M = |Q|, the total number of patterns
-    n=50  #n = |N|, the wanted number of patterns
-    W=0.1  #rate
-    sig_fig = 2
+    M=10  #M = |Q|, the total number of patterns
+    n=5  #n = |N|, the wanted number of patterns
+    W=0.2  #rate
+    eps = 5e-6
+    #sig_fig = 2
     
-    p = np.random.exponential(1,M)
-    
-    #normalize
-    p = (p/p.sum())
-    
-    #order p decreasingly
-    p.sort()
-    p = p[::-1]
-    p.dump("class_test-p.dump")
-    #p = np.load("class_test-p.dump")
+    #p = np.random.exponential(1,M)
+    #
+    ##normalize
+    #p = (p/p.sum())
+    #
+    ##order p decreasingly
+    #p.sort()
+    #p = p[::-1]
+    #p.dump("class_test-p.dump")
+    p = np.load("class_test-p.dump")
 
     exact_solver = BPV.BPV("exact",M,n,W,p)
     ex_t = timeit.timeit(exact_solver.solve,number=1)
     #exact_solver.print_solution_summary()
     #print(exact_solver.solution_feasibility())
 
-    solv = BPV.BPV("dynprog", M,n,W,p,dynprog_significant_figures=sig_fig)
-    s_t = timeit.timeit(solv.solve, number=1)
+    solv = BPV.BPV("dynprog", M,n,W,p)
+    def solve_dynprog():
+        solv.solve(eps)
+    s_t = timeit.timeit(solve_dynprog, number=1)
     solv2 = BPV.BPV("euristic", M,n,W,p)
     s2_t = timeit.timeit(solv2.solve,number=1)
     print(":::TIMES:::\nExact: ", ex_t, "\nDynProg: ", s_t, "\nEuristic: ", s2_t)
     #solv.print_solution_summary()
     #print(exact_solver.solution_entropy(), solv.solution_entropy(), solv2.solution_entropy())
     BPV.print_comparison_table(exact_solver,solv,solv2)
-    #print("exact indixes = ", exact_solver.__solution_indexes__)
-    #print("euristic extremes = ", min(solv2.__solution_indexes__), max(solv2.__solution_indexes__))
+    print("exact indixes = ", exact_solver.__solution_indexes__)
+    print("euristic extremes = ", min(solv2.__solution_indexes__), max(solv2.__solution_indexes__))
+    print("dynprog indixes = ", solv.__solution_indexes__)
     #pdb.set_trace()
+
+    
 if __name__ == "__main__":
     main()

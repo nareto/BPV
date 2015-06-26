a0d85a15bfb854a80d478116e90ee6e0bd0b99db
renato <rennabh@gmail.com>
2015-06-26 17:49:57 +0200

    found bug in heuristic

---
+++ scripts/bugheur.py
@@ -0,0 +1,20 @@
+import BPV
+import pattern_manipulation as pm
+import numpy as np
+import pandas as pd
+
+N,W = 10, 0.1504
+
+data = BPV.Data()
+data.read_csv("p.delviva.csv",False)
+data.df.sort_index(by="p",inplace=True,ascending=True)
+data.df.set_index(pd.Index([j for j in range(len(data.df))]), inplace=True)
+
+prbl_pulp = BPV.BPV("pulp",data,N,W,time_solver=False)
+prbl_pulp.solve()
+prbl_pulp.pprint_solution()
+
+
+prbl_pulp = BPV.BPV("heuristic",data,N,W,time_solver=False)
+prbl_pulp.solve()
+prbl_pulp.pprint_solution()
